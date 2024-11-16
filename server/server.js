86b1625573ead955c37dc6283bd0b0a08ee8fd1e
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-11-16 14:34:32 +0100

    fetching individual data

--- server/server.js
+++ server/server.js
@@ -39,7 +39,32 @@ app.get('/info', (req, res) => {
   res.send(`Phonebook has info for ${persons.length} people. <br> ${new Date()} `)
 })
 
-const PORT = 3000;
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+
+  const person = persons.find(person => person.id === id )
+
+  try {
+
+    // if(!person) {
+    //   console.log("person not found")
+    //   return res.status(404)
+
+    // }
+
+    // console.log(person)
+     res.status(200).json(person)
+     console.log(person)
+   
+
+  } catch (error) {
+    console.log("Error fetching data", error.message)
+    res.status(500).json( "server internal error")
+  }
+
+})
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
